68921a2d35192b2e7e781e0557cabfe9ae88e43f
Tuncay Demirler <98423535+Tuncayelif@users.noreply.github.com>
2025-11-10 20:52:40 +0300

    Implement user profile customization

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-10T17:52:34.377Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-10T17:52:39.332Z
+Commit: Implement user profile customization
