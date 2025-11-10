c8e838a7435f9092900cca3ac899d61942b8d7fe
Tuncay Demirler <98423535+Tuncayelif@users.noreply.github.com>
2025-11-10 20:52:35 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-10T17:52:31.374Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-10T17:52:34.377Z
+Commit: Fix CSS styling bug on mobile devices
